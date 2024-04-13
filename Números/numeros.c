9007bb03e58807c11d5e562eba17fa38eb67b79e
Eduardo Izidorio <99437216+EhoKira@users.noreply.github.com>
2024-04-13 14:15:53 -0400

    upload code numeros.c

---
+++ Números/numeros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i=1;
+
+    /* for(i = 1; i <= 100; i++){
+        printf("%d\n", i);
+    } */
+
+    while(i<=100) {
+        printf("%d\n",i++);
+        /* i++; */
+    }
+
+}
