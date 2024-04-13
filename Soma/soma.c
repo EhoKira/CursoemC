2263a1938dd8b7a5c3d88f758ccafd0cf4613c66
Eduardo Izidorio <99437216+EhoKira@users.noreply.github.com>
2024-04-13 14:17:55 -0400

    Create soma.c

---
+++ Soma/soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int soma = 0;
+
+    for(int i = 1; i <= 100; i++){
+        soma = soma + i;
+    }
+
+    printf("A soma dos números de 1 até 100 é %d\n", soma);
+
+    return 0;
+}
