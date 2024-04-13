f445989cc1541f91b15e8018f8147949f491b716
Eduardo Izidorio <99437216+EhoKira@users.noreply.github.com>
2024-04-13 14:08:53 -0400

    code tabuada

---
+++ Tabuada/tabuada.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int tabuada;
+
+    printf("Digite o número da tabuada: ");
+    scanf(" %d", &a);
+    printf("Tabuada de %d\n", a);
+    for(b = 1; b <= 10; b++) {
+        tabuada = a * b;
+        printf("%d x %d = %d\n", a, b, tabuada);
+    }
+}
